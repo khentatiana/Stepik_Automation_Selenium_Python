5b6fd54e613b25a2dd4bf70bfb141b72b93ab2e6
tatiana <khen.tatiana@gmail.com>
2020-03-06 00:38:28 -0800

    3.2 step 13 practices

--- Stepik_Auto_QA/test_3_2_step13_REDO_Practice_03_unittest.py
+++ Stepik_Auto_QA/test_3_2_step13_REDO_Practice_03_unittest.py
@@ -7,3 +7,37 @@ class TestRegistragionPage(unittest.TestCase):
     def setUp(self) -> None:
         self.driver = webdriver.Chrome()
 
+    def fillout_form(self, link):
+        browser = self.driver
+        browser.implicitly_wait(5)
+        browser.get(link)
+
+        browser.find_element_by_class_name(".first_block .first").send_keys("Tanya")
+        browser.find_element_by_class_name(".first_block .second").send_keys("Khen")
+        browser.find_element_by_class_name("form-control.third").send_keys("t@gmail.com")
+
+        button = browser.find_element_by_class_name("btn.btn-default").click()
+
+        time.sleep(1)
+
+        welcome_text_elem = browser.find_elements_by_tag_name("h1")
+        welcome_text = welcome_text_elem[0].text
+        return welcome_text
+
+     def test_registration_positive(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        registration_result = self.fillout_form(link)
+
+        self.assertEqual("Congratulations! You have successfully registered!", registration_result)
+
+    def test_registration_negative(self):
+        link = "http://suninjuly.github.io/registration2.html"
+        registration_result = self.fillout_form(link)
+
+        self.assertEqual("Congratulations! You have successfully registered!", registration_result)
+
+
+    def tearDown(self) -> None:
+        self.driver.quit()
+if __name__ == "__main__":
+    unittest.main()
