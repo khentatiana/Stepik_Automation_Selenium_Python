7cf089f3737a1e2a304cd39e25a3153223fc79d2
tatiana <khen.tatiana@gmail.com>
2020-03-05 23:42:39 -0800

    3.2 step 13 practices

--- Stepik_Auto_QA/3.2_step13_Задание: оформляем тесты в стиле unittest.py
+++ Stepik_Auto_QA/3.2_step13_Задание: оформляем тесты в стиле unittest.py
@@ -2,6 +2,7 @@ import unittest
 from selenium import webdriver
 import time
 
+
 class RegistrationTests(unittest.TestCase):
     def test_positive(self):
         link = "http://suninjuly.github.io/registration1.html"
@@ -10,7 +11,7 @@ class RegistrationTests(unittest.TestCase):
 
         input1 = browser.find_element_by_xpath("//input[@placeholder='Input your first name']")
         input1.send_keys("Ivan")
-        browser.find_element_by_xpath("//input[@placeholder='Input your last name']")\
+        browser.find_element_by_xpath("//input[@placeholder='Input your last name']") \
             .send_keys("Petrov")
         browser.find_element_by_class_name("form-control.third").send_keys("ivan@gmail.com")
 
@@ -49,5 +50,7 @@ class RegistrationTests(unittest.TestCase):
 
         self.assertEqual(welcome_text, "Congratulations! You have successfully registered!", "Registration is FAILED")
         browser.quit()
+
+
 if __name__ == "__main__":
     unittest.main()
