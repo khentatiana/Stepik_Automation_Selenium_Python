691570c9f52310f854cde84e00ed137177a85924
tatiana <khen.tatiana@gmail.com>
2020-03-07 21:27:15 -0800

    3.4_step 6 fixture and autouse=True

---
+++ Stepik_Auto_QA/test_3_4_step07_PyTest_Задание___область_видимости_фикстур.py
@@ -0,0 +1,36 @@
+import pytest
+from selenium import webdriver
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+@pytest.fixture(scope="class")
+def prepare_faces():
+    print("^_^", "\n")
+    yield
+    print(":3", "\n")
+
+
+@pytest.fixture()
+def very_important_fixture():
+    print(":)", "\n")
+
+
+@pytest.fixture(autouse=True)
+def print_smiling_faces():
+    print(":-Р", "\n")
+
+
+class TestPrintSmilingFaces():
+    def test_first_smiling_faces(self, prepare_faces, very_important_fixture):
+        # какие-то проверки
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector("#login_link")
+        print("perform login test")
+        pass
+
+    def test_second_smiling_faces(self, prepare_faces):
+        # какие-то проверки
+        self.browser.get(link)
+        self.browser.find_element_by_css_selector("#login_link")
+        print("perform login test")
+        pass
