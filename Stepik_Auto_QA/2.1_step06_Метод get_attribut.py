c063f06e2b8b005ffedd4405631f733b34e34234
tatiana <khen.tatiana@gmail.com>
2021-06-07 00:05:06 -0700

    Initial commit

--- Stepik_Auto_QA/2.1_step06_Метод get_attribut.py
+++ Stepik_Auto_QA/2.1_step06_Метод get_attribut.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 link = "http://suninjuly.github.io/math.html"
 
@@ -6,8 +7,11 @@ browser = webdriver.Chrome()
 browser.implicitly_wait(2)
 browser.get(link)
 
+'''Find element with id = robotCheckbox'''
 checkbox = browser.find_element_by_id("robotCheckbox")
+'''Save attribute "required" to  "checkbox_required"'''
 checkbox_required = checkbox.get_attribute("required")
+'''Assert that  attribute "required" is exists'''
 assert checkbox_required == "true", "Required is NOT present"
 
 people_rule = browser.find_element_by_id("peopleRule")
@@ -20,4 +24,5 @@ robots_checked = robots_rule.get_attribute("checked")
 
 assert robots_checked is None, "Robots rule is not checked"
 
+time.sleep(100)
 browser.quit()
